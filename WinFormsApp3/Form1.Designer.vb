723acd94c3a2e81691416ec46e551e28bd7a379e
ahmetservet <ahmetservet04@gmail.com>
2023-03-07 02:16:46 +0300

    First release of my Diary App

---
+++ WinFormsApp3/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.diaryTextBox = New System.Windows.Forms.RichTextBox()
+        Me.diaryList = New System.Windows.Forms.ComboBox()
+        Me.diaryName = New System.Windows.Forms.TextBox()
+        Me.directoryLabel = New System.Windows.Forms.Label()
+        Me.newDiary = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.Window
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.Location = New System.Drawing.Point(703, 74)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 32)
+        Me.Button2.TabIndex = 23
+        Me.Button2.Text = "Kaydet"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.Window
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.Location = New System.Drawing.Point(703, 112)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 32)
+        Me.Button3.TabIndex = 24
+        Me.Button3.Text = "Sil"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.Window
+        Me.Button4.Location = New System.Drawing.Point(704, 413)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(74, 25)
+        Me.Button4.TabIndex = 30
+        Me.Button4.Text = "Çıkış"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 423)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Karakter Sayısı:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(117, 423)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "0"
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.SystemColors.Window
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button5.Location = New System.Drawing.Point(704, 171)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(30, 33)
+        Me.Button5.TabIndex = 27
+        Me.Button5.Text = "A"
+        Me.Button5.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.SystemColors.Window
+        Me.Button6.Location = New System.Drawing.Point(731, 171)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(30, 33)
+        Me.Button6.TabIndex = 28
+        Me.Button6.Text = "A"
+        Me.Button6.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.SystemColors.Window
+        Me.Button7.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button7.Location = New System.Drawing.Point(704, 210)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(28, 25)
+        Me.Button7.TabIndex = 29
+        Me.Button7.Text = "K"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'SaveFileDialog1
+        '
+        '
+        'diaryTextBox
+        '
+        Me.diaryTextBox.Location = New System.Drawing.Point(26, 36)
+        Me.diaryTextBox.Name = "diaryTextBox"
+        Me.diaryTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical
+        Me.diaryTextBox.Size = New System.Drawing.Size(661, 378)
+        Me.diaryTextBox.TabIndex = 22
+        Me.diaryTextBox.Text = ""
+        '
+        'diaryList
+        '
+        Me.diaryList.FormattingEnabled = True
+        Me.diaryList.Location = New System.Drawing.Point(26, 7)
+        Me.diaryList.Name = "diaryList"
+        Me.diaryList.Size = New System.Drawing.Size(175, 23)
+        Me.diaryList.TabIndex = 20
+        '
+        'diaryName
+        '
+        Me.diaryName.Location = New System.Drawing.Point(207, 7)
+        Me.diaryName.Name = "diaryName"
+        Me.diaryName.Size = New System.Drawing.Size(480, 23)
+        Me.diaryName.TabIndex = 21
+        '
+        'directoryLabel
+        '
+        Me.directoryLabel.AutoSize = True
+        Me.directoryLabel.Location = New System.Drawing.Point(169, 423)
+        Me.directoryLabel.Name = "directoryLabel"
+        Me.directoryLabel.Size = New System.Drawing.Size(0, 15)
+        Me.directoryLabel.TabIndex = 13
+        '
+        'newDiary
+        '
+        Me.newDiary.Location = New System.Drawing.Point(704, 36)
+        Me.newDiary.Name = "newDiary"
+        Me.newDiary.Size = New System.Drawing.Size(74, 32)
+        Me.newDiary.TabIndex = 25
+        Me.newDiary.Text = "Yeni"
+        Me.newDiary.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Window
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.newDiary)
+        Me.Controls.Add(Me.directoryLabel)
+        Me.Controls.Add(Me.diaryName)
+        Me.Controls.Add(Me.diaryList)
+        Me.Controls.Add(Me.diaryTextBox)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Diary App"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents diaryTextBox As RichTextBox
+    Friend WithEvents diaryList As ComboBox
+    Friend WithEvents diaryName As TextBox
+    Friend WithEvents directoryLabel As Label
+    Friend WithEvents newDiary As Button
+End Class
---
+++ WinFormsApp3/Form1.vb
@@ -0,0 +1,156 @@
+﻿Imports System.Diagnostics.Eventing
+Imports System.IO
+Imports System.Runtime.CompilerServices
+
+Public Class Form1
+
+    Dim folderPath As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\DiaryApp"
+    Private Sub CreateFolder()
+        If Not Directory.Exists(folderPath) Then
+            Directory.CreateDirectory(folderPath)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim text As String = diaryTextBox.Text
+        Dim filePath As String = folderPath & "\" & diaryName.Text & ".txt"
+        directoryLabel.Text = filePath
+        My.Computer.FileSystem.WriteAllText(filePath, text, False)
+
+
+        diaryNames()
+        diaryList.Text = diaryName.Text
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Close()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If diaryList.Items.Contains(diaryName.Text) Then
+            Dim Response As Integer = MsgBox("Günlüğünüzü Silmek İstiyor Musunuz?", vbYesNo, "Diary App")
+
+            If Response = vbYes Then
+                Dim filePath As String = folderPath & "\" & diaryName.Text & ".txt"
+                My.Computer.FileSystem.DeleteFile(filePath)
+
+                diaryNames()
+
+            End If
+        End If
+        diaryTextBox.Clear()
+        diaryName.Clear()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CreateFolder()
+        diaryNames()
+    End Sub
+
+    Private Sub diaryNames()
+        diaryList.Text = ""
+        diaryList.Items.Clear()
+
+        Dim fileNames As String() = System.IO.Directory.GetFiles(folderPath)
+        For Each fileName As String In fileNames
+            Dim str = System.IO.Path.GetFileName(fileName)
+            diaryList.Items.Add(str.Substring(0, str.Length - 4))
+        Next
+    End Sub
+    Private Sub Button3_SizeChanged(sender As Object, e As EventArgs) Handles Button3.SizeChanged
+
+    End Sub
+
+    Private Sub Form1_SizeChanged(sender As Object, e As EventArgs) Handles MyBase.SizeChanged
+
+
+        newDiary.Left = Me.Size.Width - 115
+        Button2.Left = Me.Size.Width - 115
+        Button3.Left = Me.Size.Width - 115
+
+        Button4.Left = Me.Size.Width - 115
+        Button4.Top = Me.Size.Height - 70
+
+
+        diaryTextBox.Width = Me.Size.Width - 150
+        diaryTextBox.Height = Me.Size.Height - 110
+
+
+
+
+        Label1.Top = Me.Size.Height - 65
+        Label2.Top = Me.Size.Height - 65
+        directoryLabel.Top = Me.Size.Height - 65
+
+        Button5.Left = Me.Size.Width - 115
+        Button6.Left = Me.Size.Width - 85
+
+        Button7.Left = Me.Size.Width - 115
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim fontSize As Integer = diaryTextBox.SelectionFont.Size
+        fontSize += 1
+        diaryTextBox.SelectionFont = New Font(diaryTextBox.Font.Name, fontSize, diaryTextBox.SelectionFont.Style)
+
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim fontSize As Integer = diaryTextBox.SelectionFont.Size
+        fontSize -= 1
+        diaryTextBox.SelectionFont = New Font(diaryTextBox.Font.Name, fontSize, diaryTextBox.SelectionFont.Style)
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim font As Font = diaryTextBox.SelectionFont
+        If font IsNot Nothing Then
+            Dim newFontStyle As FontStyle
+            If font.Bold Then
+                newFontStyle = font.Style And Not FontStyle.Bold
+            Else
+                newFontStyle = font.Style Or FontStyle.Bold
+            End If
+            diaryTextBox.SelectionFont = New Font(font.FontFamily, font.Size, newFontStyle)
+        End If
+    End Sub
+
+
+
+    Private Sub SaveFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles diaryTextBox.TextChanged
+        Label2.Text = diaryTextBox.Text.Length
+    End Sub
+
+    Private Sub diaryList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles diaryList.SelectedIndexChanged
+        diaryName.Text = diaryList.Text
+        Dim filePath As String = folderPath & "\" & diaryList.SelectedItem & ".txt"
+        diaryTextBox.Text = My.Computer.FileSystem.ReadAllText(filePath)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles newDiary.Click
+        diaryList.Text = ""
+        diaryName.Clear()
+        diaryTextBox.Clear()
+    End Sub
+End Class
